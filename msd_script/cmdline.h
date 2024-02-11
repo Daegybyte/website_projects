859baf258b8a26b7d81410eff85016b89260ee6a
Diego <lucy@lucy.local>
2024-02-10 18:02:55 -0700

    some edits to README in Distance Arbitrage

--- msd_script/cmdline.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pointer.h"
-
-/**
- * Takes in arguments from the command line
- * @param argc number of arguments
- * @param argv the argument being passed
- */
-void use_arguments(int argc, char *argv[]);
